8175e80d573eab55c7bf7b1ce42540a3b3fe3e78
Guilherme Lemmi <guilhermelemmi@gmail.com>
2021-05-24 10:50:31 -0300

    pieces collision and game over

--- src/components/TetrisGame/Game.js
+++ src/components/TetrisGame/Game.js
@@ -71,21 +71,51 @@ const shapes = {
 const n = initialBoard[0].length;
 
 const types = ["i", "j", "l", "s", "z", "t", "o"];
+const initialType = types[Math.floor(Math.random() * types.length)];
 
 function Game() {
-  let [board, setBoard] = useState(initialBoard);
-  let [pieceCoordinates, setPieceCoordinates] = useState([1, 2, 11, 12]);
-  let [pieceRotation, setPieceRotation] = useState(0);
-  let [pieceType, setPieceType] = useState("o");
-  let [nextPieceType, setNextPieceType] = useState("j");
-  let [delay, setDelay] = useState(1000);
+  let [delay] = useState(1000);
   const [isRunning, setIsRunning] = useState(true);
+  const [isOver, setIsOver] = useState(false);
+
+  let [board, setBoard] = useState(initialBoard);
+  let [pieceType, setPieceType] = useState(initialType);
+  let [pieceRotation, setPieceRotation] = useState(0);
+
+  let [pieceCoordinates, setPieceCoordinates] = useState(
+    shapes[initialType][0](n, n / 2)
+  );
+
+  let [nextPieceType, setNextPieceType] = useState(
+    types[Math.floor(Math.random() * types.length)]
+  );
+
+  const willCollide = useCallback(() => {
+    const isAtBottomRow = Math.max(...pieceCoordinates) / n >= board.length - 1;
+    return (
+      isAtBottomRow ||
+      pieceCoordinates
+        .filter((block) => block > 0)
+        .some((block) => {
+          console.log("block", block);
+          const coords = block.toString().split("");
+          console.log("coords", coords);
+          const col = parseInt(coords.pop(), 10);
+          console.log("col", col);
+          const row = coords.length ? parseInt(coords.join(""), 10) + 1 : 1;
+          console.log("row", row);
+          return board[row][col];
+        })
+    );
+  }, [pieceCoordinates, board]);
 
   const move = useCallback(
     (increment) => {
-      setPieceCoordinates(pieceCoordinates.map((block) => block + increment));
+      if (!willCollide()) {
+        setPieceCoordinates(pieceCoordinates.map((block) => block + increment));
+      }
     },
-    [pieceCoordinates]
+    [pieceCoordinates, willCollide]
   );
 
   const rotate = useCallback(() => {
@@ -117,14 +147,20 @@ function Game() {
 
   useInterval(
     () => {
-      if (!isPieceAtBottom(pieceCoordinates)) {
+      if (!willCollide()) {
         setPieceCoordinates(pieceCoordinates.map((block) => block + 10));
       } else {
         let updatedBoard = [...board];
         pieceCoordinates.forEach((block) => {
           const coords = block.toString().split("");
-          const y = parseInt(coords.pop(), 10);
-          updatedBoard[parseInt(coords.join(""), 10)][y] = pieceType;
+          const col = parseInt(coords.pop(), 10);
+          const row = parseInt(coords.join(""), 10);
+          if (updatedBoard[row]) {
+            updatedBoard[parseInt(coords.join(""), 10)][col] = pieceType;
+          } else {
+            setIsRunning(false);
+            setIsOver(true);
+          }
         });
 
         setBoard(updatedBoard);
@@ -166,9 +202,6 @@ function Game() {
     }
   }, [board]);
 
-  const isPieceAtBottom = (pieceCoordinates) =>
-    Math.max(...pieceCoordinates) / 10 >= 19;
-
   const rows = board.map((row, i) => {
     const cells = row.map((cell, j) => (
       <div
@@ -183,9 +216,12 @@ function Game() {
   });
 
   return (
-    <div tabIndex="0" className="game" onKeyDown={handleKeyDown}>
-      <div className="board">{rows}</div>
-    </div>
+    <>
+      {isOver && <h1>Game Over</h1>}
+      <div tabIndex="0" className="game" onKeyDown={handleKeyDown}>
+        <div className="board">{rows}</div>
+      </div>
+    </>
   );
 }
 
